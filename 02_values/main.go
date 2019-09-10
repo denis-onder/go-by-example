f1849ad839d75a39ea3b7c400ed330058dc3fdcb
denis-onder <denis_onder@protonmail.com>
2019-09-10 18:07:13 +0200

    Values

---
+++ 02_values/main.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("go" + "lang")          // Strings
+	fmt.Println("2 + 2 =", 2+2)         // Integers
+	fmt.Println("7.0 / 2.0 =", 7.0/2.0) // Floats
+	fmt.Println(true && false)          // Booleans
+}
