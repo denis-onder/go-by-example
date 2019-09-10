7f3014206ea2850b99fadc0d994b968172523405
denis-onder <denis_onder@protonmail.com>
2019-09-10 18:30:12 +0200

    Variables and constants

---
+++ 03_variables/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a = "initial string"
+	fmt.Println(a)
+	a = "modified string"
+	fmt.Println(a)
+	var b int
+	fmt.Println(b)
+	c := 10
+	fmt.Println(c)
+}
---
+++ 04_constants/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	const pi = 3.1415
+	fmt.Println("Two pi:", 2*pi)
+	fmt.Println("Pi squared:", math.Pow(pi, 2))
+}
